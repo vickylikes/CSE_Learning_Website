98914f8bfadfcbc560bc09a4cda0d8cfd3438328
vickylikes <vicky.at.my.website@gmail.com>
2026-08-07 11:33:36 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,54 @@
+/*
+==================================
+VickyLikes Configuration
+Do not modify unless instructed.
+==================================
+*/
+
+const CONFIG = {
+
+websiteName: "VickyLikes",
+
+version: "2.0",
+
+
+
+tree:{
+
+enabled:true,
+
+animationSpeed:1,
+
+windStrength:.3,
+
+leafGrowthAnimation:true,
+
+branchGrowthAnimation:true,
+
+clickableLeaves:true
+
+},
+
+
+
+progress:{
+
+saveLocally:true,
+
+cloudSync:false
+
+},
+
+
+
+dashboard:{
+
+showStatistics:true,
+
+showBookmarks:true,
+
+showKnowledgeTree:true
+
+}
+
+};
